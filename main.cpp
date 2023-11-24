55e5deaf3f170e77bf1816550e2ae35c59b54ba6
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:49:34 +0200

    done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,24 @@
 
 using namespace std;
 
+void removeCharOccurrences(string& str, char targetChar) {
+	str.erase(remove(str.begin(), str.end(), targetChar), str.end());
+}
+
 int main()
 {
-	cout << "Hello World!" << endl;
+    string inputString;
+    char charToRemove;
+
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    cout << "Enter the character to remove: ";
+    cin >> charToRemove;
+
+    removeCharOccurrences(inputString, charToRemove);
+
+    cout << "Result: " << inputString << endl;
+
 	return 0;
 }
