a5c9a5493489e202b7bb578db9ff5a7a2a86b294
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:53:15 +0200

    done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,29 @@
 
 using namespace std;
 
+void insertCharAtPosition(string& str, char charToInsert, int position) {
+	str.insert(position, 1, charToInsert);
+}
+
+
 int main()
 {
-	cout << "Hello World!" << endl;
+    string inputString;
+    char charToInsert;
+    int positionToInsert;
+
+    cout << "Enter a string: ";
+    cin >> inputString;
+
+    cout << "Enter the character to insert: ";
+    cin >> charToInsert;
+
+    cout << "Enter the position to insert: ";
+    cin >> positionToInsert;
+
+    insertCharAtPosition(inputString, charToInsert, positionToInsert);
+
+    cout << "Result: " << inputString << endl;
+
 	return 0;
 }
