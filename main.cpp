47d28c8ea7f35fce0388d42d1e3738ccbd1d4bb6
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:29:42 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,27 @@
 #include <iostream>
+#include <cctype>
 
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    string sentence;
+    cout << "Enter a string: ";
+    cin >> sentence;
+
+    int wordCount = 0;
+
+    for (char ch : sentence) {
+        if (isspace(ch) || ispunct(ch)) {
+            wordCount++;
+        }
+    }
+
+    if (!sentence.empty()) {
+        wordCount++;
+    }
+ 
+    cout << "Number of words: " << wordCount << endl;
+
 	return 0;
 }
