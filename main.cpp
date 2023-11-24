cb06043254a378fdbc8928dc8b5eed1370ac3740
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:56:46 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+void replaceDotsWithExclamation(string& str) {
+    size_t found = str.find('.');
+    while (found != string::npos) {
+        str.replace(found, 1, "!");
+        found = str.find('.', found + 1);
+    }
+}
+
 int main()
 {
-	cout << "Hello World!" << endl;
+    string userInput;
+
+    cout << "Enter a string: ";
+    cin >> userInput;
+
+    replaceDotsWithExclamation(userInput);
+
+    cout << "Result: " << userInput << endl;
+
 	return 0;
 }
