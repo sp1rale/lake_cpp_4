e10f9d4890a3ee9a75c2359fdceba743db830372
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 20:00:45 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include <string>
 
 using namespace std;
 
+int countOccurrences(const string& str, char targetChar) 
+{
+    int count = 0;
+    for (char ch : str) {
+        if (ch == targetChar) {
+            count++;
+        }
+    }
+    return count;
+}
+
 int main()
 {
-	cout << "Hello World!" << endl;
+    string userInput;
+    char targetCharacter;
+
+    cout << "Enter a string: ";
+    cin >> userInput;
+
+    cout << "Enter the character to count: ";
+    cin >> targetCharacter;
+
+    int occurrences = countOccurrences(userInput, targetCharacter);
+
+    cout << "The character '" << targetCharacter << "' occurs " << occurrences << " times in the string." << endl;
+
 	return 0;
 }
