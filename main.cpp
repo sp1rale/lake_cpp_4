5d6a909f8fb9f7f975fcec6f9ca73ab02a762aa3
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 20:04:34 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+void countCharacters(const string& str, int& letterCount, int& digitCount, int& otherCount) {
+    for (char ch : str) {
+        if (isalpha(ch)) {
+            letterCount++;
+        }
+        else if (isdigit(ch)) {
+            digitCount++;
+        }
+        else {
+            otherCount++;
+        }
+    }
+}
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    string userInput;
+    int letterCount = 0, digitCount = 0, otherCount = 0;
+
+    cout << "Enter a string: ";
+    cin >> userInput;
+
+    countCharacters(userInput, letterCount, digitCount, otherCount);
+
+    cout << "Letters: " << letterCount << endl;
+    cout << "Digits: " << digitCount << endl;
+    cout << "Other characters: " << otherCount << endl;
+
 	return 0;
 }
