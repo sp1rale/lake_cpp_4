9d624e9c14c72d1afacd3ac3cace4fcc112c7589
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:37:51 +0200

    done

--- main.cpp
+++ main.cpp
@@ -2,8 +2,44 @@
 
 using namespace std;
 
+bool isPalindrome(const string& str) {
+    int i = 0;
+    int j = str.length() - 1;
+
+    while (i < j) {
+
+        while (i < j && !isalnum(str[i])) {
+            i++;
+        }
+        while (i < j && !isalnum(str[j])) {
+            j--;
+        }
+
+        if (tolower(str[i]) != tolower(str[j])) {
+            return false;
+        }
+
+        i++;
+        j--;
+    }
+
+    return true;
+}
+
+
 int main()
 {
-	cout << "Hello World!" << endl;
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    if (isPalindrome(str)) {
+        cout << "A string is a palindrome." << endl;
+    }
+    else {
+        cout << "A string is a not palindrome." << endl;
+    }
+
+
 	return 0;
 }
