1a09f11e294e1c4ad4ca3e7d84fee159e706a1e7
sp1rale <dimayaremchyk24@gmail.com>
2023-11-24 19:13:34 +0200

    done

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
 #include <iostream>
 
+
 using namespace std;
 
 int main()
 {
-	cout << "Hello World!" << endl;
+    string str;
+    cout << "Enter a string: ";
+    cin >> str;
+
+    int letters = 0;
+    int digits = 0;
+    int others = 0;
+
+    for (char ch : str) {
+        if (isalpha(ch)) {
+            letters++;
+        }
+        else if (isdigit(ch)) {
+            digits++;
+        }
+        else {
+            others++;
+        }
+    }
+
+   
+    cout << "Letters: " << letters << endl;
+    cout << "Digits: " << digits << endl;
+    cout << "Other characters: " << others << endl;
+
 	return 0;
 }
